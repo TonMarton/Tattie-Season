c1afe926a2743453930b5184203c947da45f8565
Alexandros Tselikas <alexandrostselikas@Alexandross-MacBook-Pro.local>
2023-01-08 15:16:37 +0200

    camera boundaries

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,10 +6,20 @@ public class CameraFollow : MonoBehaviour
 {
   
     public Transform target;
-    public int offset = 10;
+    public Vector3 offset;
+    [Range(1,10)]
+    public float smoothFactor;
+    public Vector3 minValue,maxValue;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y + offset, -10);
+        Follow();
+    }
+    void Follow(){
+        Vector3 targetPosition = target.position + offset;
+        Vector3 clampedPosition = new Vector3(Mathf.Clamp(targetPosition.x, minValue.x, maxValue.x), Mathf.Clamp(targetPosition.y, minValue.y, maxValue.y), Mathf.Clamp(targetPosition.z, minValue.z, maxValue.z));
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, clampedPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = smoothPosition;
     }
 }
