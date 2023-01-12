45a3ba4c7449605a73b53cf1e21370ac1aaf4988
Márton Nagy <ton.marton@gmail.com>
2023-01-12 13:30:34 +0100

    feat: redo main menu UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,21 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    [SerializeField] private UnityEvent playButtonHoverSound;
+    [SerializeField] private UnityEvent playButtonClickSound;
+
+    private UIDocument uIDocument;
+
+    private VisualElement rootElement;
+    private Button playButton;
+    private Button quitButton;
+
+    private void Awake()
     {
+        uIDocument = gameObject.GetComponent<UIDocument>();
+
+        rootElement = uIDocument.rootVisualElement;
+        playButton = rootElement.Q<Button>("play-button");
+        quitButton = rootElement.Q<Button>("quit-button");
+
+        playButton.clickable.clicked += OnPlayButtonClicked;
+        quitButton.clickable.clicked += OnQuitButtonClicked;
+
+        playButton.RegisterCallback<MouseOverEvent>((type) => OnButtonHover());
+        quitButton.RegisterCallback<MouseOverEvent>((type) => OnButtonHover());
+
+    }
+
+    private void OnButtonHover() {
+        playButtonHoverSound.Invoke();
+    }
+
+    private void OnButtonClicked() {
+        playButtonClickSound.Invoke();
+    }
+
+    private void OnPlayButtonClicked()
+    {
+        OnButtonClicked();
         SceneManager.LoadScene(1);
     }
 
-    public void QuitGame()
+    private void OnQuitButtonClicked()
     {
+        OnButtonClicked();
+
         #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
         #endif
 
         Application.Quit();
+    }
+
+    private void OnDestroy()
+    {
+        playButton.clickable.clicked -= OnPlayButtonClicked;
+        quitButton.clickable.clicked -= OnQuitButtonClicked;
+
+        playButton.UnregisterCallback<MouseOverEvent>((type) => OnButtonHover());
+        quitButton.UnregisterCallback<MouseOverEvent>((type) => OnButtonHover());
     }
 }
